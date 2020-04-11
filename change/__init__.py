560568edda6cd3cb708952c16fe721d4343f80ee
Sarath C Akurathi <sarath.raja003@gmail.com>
2020-04-11 19:04:37 +0530

    Update __init__.py

--- change/__init__.py
+++ change/__init__.py
@@ -1,1 +1,10 @@
+from change.main import ChangeManager
+
+
+change_manager = ChangeManager()
+
+def create_change_request(template_id, params):
+    response = change_manager.create_cr(template_id=template_id, params=params)
+
+    return response
 
